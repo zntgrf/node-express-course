c7baaab91d911a373f78cd0d64212e9458eb83a5
Sebastian Zentgraf <info@zntgrf.dev>
2020-03-21 01:14:06 +0100

    add bodyparser to server and post route

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json());
 const mockUserData=[
     {name:'Mark'},
     {name:'Jill'}
@@ -26,4 +28,24 @@ app.listen(8000,function() {
     console.log('server is running')
 })
 
+app.post('/login',function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+
+    const mockUsername="billyTheKid";
+    const mockPassword="superSecret";
+
+    if (username===mockUsername && password===mockPassword){
+         res.json({
+              success: true,
+              message: 'password and username match!',
+              token: 'encrypted token goes here'
+         })
+    } else {
+         res.json({
+              success: false,
+              message: 'password and username do not match'
+         })
+    }
+})
 
